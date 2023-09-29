19808875fb8427e50fd93994cec228646287fcc3
Orozco Oscar <88407386+OrozcoOscar@users.noreply.github.com>
2023-09-29 07:50:30 -0500

    Update indexedDb.js

--- indexedDb.js
+++ indexedDb.js
@@ -1,8 +1,8 @@
 /**
  * OrozcoOscar
- * v1.0
- * 22/08/23
- */
+ * v2.0
+ * 23/09/23
+ */ 
 
 class IndexDB {
     /** 
@@ -12,87 +12,139 @@ class IndexDB {
      */
     constructor(nameDB, ver) {
         this.tables = [];
-        this.con = null;
         this.db = null;
+        this.nameDB = nameDB;
+        this.ver = 1;
 
-        this.connectCallback = () => { };
 
-        this.con = indexedDB.open(nameDB, ver);
-
-        this.con.onupgradeneeded = () => {
-            this.db = this.con.result;
-            this.tables.forEach(table => {
-                const options = {
-                    keyPath: table.key || 'id',
-                    autoIncrement: table.autoIncrement === undefined ? true : table.autoIncrement,
-                };
-                this.db.createObjectStore(table.tabla, options);
+        this.openDatabase(nameDB, ver)
+            .then(db => {
+                this.db = db;
+            })
+            .catch(error => {
+                console.error('Error al abrir la base de datos:', error);
             });
-        };
-
-        this.con.onsuccess = () => {
-            this.db = this.con.result;
-            this.connectCallback(this);
-        };
     }
 
-    createTables(tables) {
+    openDatabase(nameDB, ver) {
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open(nameDB, ver);
+
+            request.onupgradeneeded = () => {
+                const db = request.result;
+                this.tables.forEach(table => {
+                    const options = {
+                        keyPath: table.key || 'id',
+                        autoIncrement: table.autoIncrement === undefined ? true : table.autoIncrement,
+                    };
+                    db.createObjectStore(table.tabla, options);
+                });
+            };
+
+            request.onsuccess = () => {
+                resolve(request.result);
+            };
+
+            request.onerror = () => {
+                reject(request.error);
+            };
+        });
+    }
+
+    async createTables(tables) {
         this.tables = tables;
 
         if (this.db) {
-            this.tables.forEach(table => {
+            this.tables?.forEach(table => {
                 const options = {
                     keyPath: table.key || 'id',
                     autoIncrement: table.autoIncrement === undefined ? true : table.autoIncrement,
                 };
-                this.db.createObjectStore(table.tabla, options);
+                this.db?.createObjectStore(table.tabla, options);
             });
         }
     }
 
-    connect(callback) {
-        this.connectCallback = callback;
-        this.connectCallback(this);
+    async connect(callback) {
+        await this.openDatabase(this.nameDB, this.ver)
+        return new Promise((resolve, reject) => {
+            resolve(callback())
+        })
     }
 
-    // Rest of the methods remain the same...
-
     async removeAll(tabla) {
+        await this.openDatabase(this.nameDB, this.ver)
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
         try {
-            const transaction = this.db.transaction([tabla], 'readwrite');
-            const store = transaction.objectStore(tabla);
-            await store.clear();
-            return { status: true };
+            const transaction = this.db?.transaction([tabla], 'readwrite');
+            const store = transaction?.objectStore(tabla);
+            return new Promise(async (resolve, reject) => {
+                await store.clear()
+                resolve({ status: true, message: 'TABLA ELIMINADA' })
+            });
         } catch (error) {
             return { status: false, error };
         }
     }
 
     async remove(tabla, id) {
+        await this.openDatabase(this.nameDB, this.ver)
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
         try {
-            const transaction = this.db.transaction([tabla], 'readwrite');
-            const store = transaction.objectStore(tabla);
-            await store.delete(id);
-            return { status: true };
+            const transaction = this.db?.transaction([tabla], 'readwrite');
+            const store = transaction?.objectStore(tabla);
+            return new Promise((resolve, reject) => {
+                const request = store.get(id);
+
+                request.onsuccess = async (e) => {
+                    if (e.target.result) {
+                        await store?.delete(id);
+                        resolve({ status: true, data: e.target.result, message: 'Elemento eliminado' });
+                    } else {
+                        resolve({ status: false, data: null, message: 'El elemento NO existe' });
+                    }
+                };
+                request.onerror = (e) => {
+                    reject({ status: false, error: e });
+                };
+            });
         } catch (error) {
             return { status: false, error };
         }
     }
 
     async get(tabla, id) {
+        await this.openDatabase(this.nameDB, this.ver)
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
         const transaction = this.db?.transaction([tabla], 'readwrite');
         const store = transaction?.objectStore(tabla);
 
         return new Promise((resolve, reject) => {
             const request = store.get(id);
-            
+
             request.onsuccess = (e) => {
-                if(e.target.result){
+                if (e.target.result) {
                     resolve({ status: true, data: e.target.result });
-                }else{
+                } else {
                     resolve({ status: false, data: null });
                 }
-                
             };
             request.onerror = (e) => {
                 reject({ status: false, error: e });
@@ -100,12 +152,19 @@ class IndexDB {
         });
     }
 
+
     async getAll(tabla) {
-        const transaction = this.db.transaction([tabla]);
-        const store = transaction.objectStore(tabla);
+        await this.openDatabase(this.nameDB, this.ver)
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
+        const transaction = this.db?.transaction([tabla]);
+        const store = transaction?.objectStore(tabla);
 
         return new Promise((resolve, reject) => {
-            const request = store.getAll();
+            const request = store?.getAll();
             request.onsuccess = (e) => {
                 const t = e.target.result;
                 const source = e.target.source;
@@ -125,60 +184,107 @@ class IndexDB {
         });
     }
 
-   async set(tabla, data) {
-    try {
-        const transaction = this.db?.transaction([tabla], 'readwrite');
-        const store = transaction?.objectStore(tabla);
-        return new Promise((resolve, reject) => {
-            const request = store.get(data.id);
-            
-            request.onsuccess = async (e) => {
-                if(e.target.result){
-                   resolve({ status: false, data: e.target.result, message: 'El elemento ya existe' });
-                }else{
-                     let result = await store?.add(data);
-                resolve({ status: true, data, message: 'Elemento agregado' });
-                }
-                
-            };
-            request.onerror = (e) => {
-                reject({ status: false, error: e });
-            };
-        });
-        
-         
-    } catch (error) {
-        return { status: false, error };
-    }
-}
+    async set(tabla, data) {
+        await this.openDatabase(this.nameDB, this.ver)
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
+        try {
+            const transaction = this.db?.transaction([tabla], 'readwrite');
+            const store = transaction?.objectStore(tabla);
+            return new Promise((resolve, reject) => {
+                const request = store.get(data._id);
 
+                request.onsuccess = async (e) => {
+                    if (e.target.result) {
+                        resolve({ status: false, data: e.target.result, message: 'El elemento ya existe' });
+                    } else {
+                        let result = await store?.add(data);
+                        resolve({ status: true, data, message: 'Elemento agregado' });
+                    }
+                };
+                request.onerror = (e) => {
+                    reject({ status: false, error: e });
+                };
+            });
+        } catch (error) {
+            return { status: false, error };
+        }
+    }
 
     async update(tabla, data) {
-            try {
-                const transaction = this.db?.transaction([tabla], 'readwrite');
-                const store = transaction?.objectStore(tabla);
-                
-                return new Promise((resolve, reject) => {
-                    const request = store.get(data.id);
-                    
-                    request.onsuccess = async (e) => {
-                        if(e.target.result){
-                                let result = await store?.put(data);
+        await this.openDatabase(this.nameDB, this.ver)
+        if (!this.db) {
+            return new Promise((resolve, reject) => {
+                resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+            });
+        }
+        try {
+            const transaction = this.db?.transaction([tabla], 'readwrite');
+            const store = transaction?.objectStore(tabla);
+
+            return new Promise((resolve, reject) => {
+                const request = store.get(data._id);
+
+                request.onsuccess = async (e) => {
+                    if (e.target.result) {
+                        let result = await store?.put(data);
                         resolve({ status: true, data, message: 'Elemento actualizado' });
-                         
-                        }else{
-                           resolve({ status: false, data: null, message: 'El elemento NO existe' });
-                        }
-                        
-                    };
-                    request.onerror = (e) => {
-                        reject({ status: false, error: e });
-                    };
-                });
-                
-                 
-            } catch (error) {
-                return { status: false, error };
-            }
+                    } else {
+                        resolve({ status: false, data: null, message: 'El elemento NO existe' });
+                    }
+                };
+                request.onerror = (e) => {
+                    reject({ status: false, error: e });
+                };
+            });
+        } catch (error) {
+            return { status: false, error };
+        }
     }
+
+    async getSongByName(tabla, name) {
+        await this.openDatabase(this.nameDB, this.ver);
+      
+        if (!this.db) {
+          return new Promise((resolve, reject) => {
+            resolve({ status: false, error: "NO SE ENCONTRÓ DB" });
+          });
+        }
+      
+        return new Promise((resolve, reject) => {
+          const transaction = this.db?.transaction([tabla], "readonly");
+          const store = transaction?.objectStore(tabla);
+      
+          const request = store.openCursor();
+      
+          const results = [];
+      
+          // Convierte el nombre de búsqueda a minúsculas
+          const searchNameLowercase = name.toLowerCase();
+      
+          request.onsuccess = (e) => {
+            const cursor = e.target.result;
+            if (cursor) {
+              const song = cursor.value;
+              // Convierte el nombre de la canción a minúsculas antes de comparar
+              if (song.name.toLowerCase().includes(searchNameLowercase)) {
+                results.push(song);
+              }
+              cursor.continue();
+            } else {
+              resolve({ status: true, data: results });
+            }
+          };
+      
+          request.onerror = (e) => {
+            reject({ status: false, error: e });
+          };
+        });
+      }
+      
+
+
 }
